127d76715bff1da1db4707e456e4e065c216d083
cih <139817646+ghstlei@users.noreply.github.com>
2023-10-04 16:01:08 -0300

    Add files via upload

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -23,6 +23,7 @@ export class DashboardComponent implements OnInit{
     this.getAllEmployee();
 
     this.empDetail = this.formBuilder.group({
+      id : [''],
       client_id : [''],
       name : [''],
       cpf : [''],
@@ -41,14 +42,15 @@ export class DashboardComponent implements OnInit{
 
     console.log(this.empDetail);
     this.empObj.id = this.empDetail.value.id;
-    this.empObj.client_id = this.empDetail.value.id;
-    this.empObj.name = this.empDetail.value.id;
-    this.empObj.national_identity = this.empDetail.value.id;
-    this.empObj.birthdate = this.empDetail.value.id;
-    this.empObj.job_position = this.empDetail.value.id;
-    this.empObj.activity = this.empDetail.value.id;
-    this.empObj.salary = this.empDetail.value.id;
-    this.empObj.email = this.empDetail.value.id;
+    this.empObj.client_id = this.empDetail.value.client_id;
+    this.empObj.name = this.empDetail.value.name;
+    this.empObj.national_identity = this.empDetail.value.national_identity;
+    this.empObj.birthdate = this.empDetail.value.birthdate;
+    this.empObj.job_position = this.empDetail.value.job_position;
+    this.empObj.activity = this.empDetail.value.activity;
+    this.empObj.salary = this.empDetail.value.salary;
+    this.empObj.type = this.empDetail.value.type;
+    this.empObj.email = this.empDetail.value.email;
 
 
     this.empService.addEmployee(this.empObj).subscribe(res=>{
@@ -67,6 +69,18 @@ export class DashboardComponent implements OnInit{
     },err=>{
       console.log("error while fetching data.")
     });
+  }
+  editEmployee(emp : Employee){
+    this.empDetail.controls['id'].setValue(emp.id);
+    this.empDetail.controls['client_id'].setValue(emp.client_id);
+    this.empDetail.controls['name'].setValue(emp.name);
+    this.empDetail.controls['national_identity'].setValue(emp.national_identity);
+    this.empDetail.controls['birthdate'].setValue(emp.birthdate);
+    this.empDetail.controls['job_position'].setValue(emp.job_position);
+    this.empDetail.controls['activity'].setValue(emp.activity);
+    this.empDetail.controls['salary'].setValue(emp.salary);
+    this.empDetail.controls['type'].setValue(emp.type);
+    this.empDetail.controls['email'].setValue(emp.email);
   }
 
 }
